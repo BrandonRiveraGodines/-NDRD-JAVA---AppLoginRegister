a45e9298a4a2720624811422361a938e5bc99f7a
Brandon Rivera Godines <brandonriveragodines@hotmail.com>
2019-02-18 20:56:55 -0600

    Summary
    First commit

---
+++ AppLoginRegister/app/src/main/java/com/brndnrg/apploginregister/app/AppConfig.java
@@ -0,0 +1,8 @@
+package com.brndnrg.apploginregister.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.4/login/login/";
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.4/login/register/";
+}
